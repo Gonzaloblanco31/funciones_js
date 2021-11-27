7dadbd695c52daa600060f5b64f69df6f6731c0c
Manolo <gonzaloblanco2012@gmail.com>
2021-11-27 11:15:13 +0100

    ACABO DE SUBIR MI ARCHIVO

---
+++ funciones.js
@@ -0,0 +1,23 @@
+function sumaDeNumeros(numeroUno, numeroDos){
+    return console.log (numeroUno+ numeroDos)
+
+}
+sumaDeNumeros (4666667866, 7986767776)
+
+function restadenumeros (restaUno, restaDos){
+    return console.log (restaUno- restaDos)
+}
+restadenumeros (1, 90)
+    function multiplicaciondenumeros (multiuno, multidos){
+    return console.log (multiuno* multidos)
+    }
+multiplicaciondenumeros(786798798789797979878798798798789798079879897890797089968978978623456789087213456700754567834579, 78970877897087808087087908780707097089079870987089078787878987978997987989797987997)
+
+function bAtAlLaEpika (personajeguan, personajetu){
+    return console.log ('lA bAtAlLa De HoY eS dE',personajeguan,'VS', personajetu)
+}
+bAtAlLaEpika ('Calvo' , 'DoraLaPerreadora')
+
+
+
+bAtAlLaEpika ('Calviss' , 'DoraLaPerreidora')
