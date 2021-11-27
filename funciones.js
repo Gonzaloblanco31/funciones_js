acde0a7c7fe9860809298ac0f574675410498c4f
Manolo <gonzaloblanco2012@gmail.com>
2021-11-27 11:21:29 +0100

    HE CAMBIADO HOY POR MAÑANA

--- funciones.js
+++ funciones.js
@@ -14,7 +14,7 @@ restadenumeros (1, 90)
 multiplicaciondenumeros(786798798789797979878798798798789798079879897890797089968978978623456789087213456700754567834579, 78970877897087808087087908780707097089079870987089078787878987978997987989797987997)
 
 function bAtAlLaEpika (personajeguan, personajetu){
-    return console.log ('lA bAtAlLa De HoY eS dE',personajeguan,'VS', personajetu)
+    return console.log ('lA bAtAlLa De maÑaNa eS dE',personajeguan,'VS', personajetu)
 }
 bAtAlLaEpika ('Calvo' , 'DoraLaPerreadora')
 
